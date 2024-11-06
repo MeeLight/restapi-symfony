4ca7d0b5bc8527343664e5e25f675e639acdffac
MeeLight <moisesreyes0112@gmail.com>
2024-11-06 01:39:00 -0400

    feat: deleteById method added

--- src/Interface/IMobilePaymentBeneficiaryService.php
+++ src/Interface/IMobilePaymentBeneficiaryService.php
@@ -9,6 +9,6 @@ interface IMobilePaymentBeneficiaryService
     public function getAll();
     public function getById(string $id);
     public function create(MobilePaymentBeneficiary $beneficiary);
-
     public function update();
+    public function delete(string $id);
 }
